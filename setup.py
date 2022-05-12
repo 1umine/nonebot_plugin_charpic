ba7fdf8c81dea4236bb3f599417ebb103101ae19
Laffey <66518048+RafuiiChan@users.noreply.github.com>
2022-05-12 21:22:19 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nonebot_plugin_charpic",
+    version="0.0.1",
+    keywords=["pip", "nonebot_plugin_charpic"],
+    description="Generate picture(or GIF) by character",
+    long_description="Generate picture(or GIF) by character, for nonebot2 beta1",
+    license="MIT Licence",
+    url="https://github.com/RafuiiChan/nonebot_plugin_charpic",
+    author="Yuyu1628", 
+    author_email="a1628420979@163.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "pillow >= 8.4.0",
+        "aiohttp",
+        "imageio >= 2.9.0",
+        "nonebot2 >= 2.0.0b1",
+        "nonebot-adapter-onebot >= 2.0.0b1",
+    ],
+)
