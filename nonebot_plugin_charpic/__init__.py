31d193b5fc8534e927b2ba4c478056dbef3c98a4
1umine <lyyme34@gmail.com>
2024-07-10 13:53:59 +0800

    Revert "feat: 支持多适配器"
    This reverts commit b66b1db24662b470a8a303fc2e6235124aec17b2.

--- nonebot_plugin_charpic/__init__.py
+++ nonebot_plugin_charpic/__init__.py
@@ -1,12 +1,10 @@
-from typing import List, Type
 from nonebot import on_command
-from nonebot.adapters import Bot, Message, MessageSegment
+from nonebot.adapters.onebot.v11 import MessageSegment, Message
 from nonebot.typing import T_State
 from nonebot.params import CommandArg
 from nonebot.plugin import PluginMetadata
 
-from .data_source import get_img, char_gif, get_pic_text, text2img
-from .adapter import Image
+from .data_source import *
 
 
 __plugin_meta__ = PluginMetadata(
@@ -15,12 +13,7 @@ __plugin_meta__ = PluginMetadata(
     usage="/字符画 <图片>",
     type="application",
     homepage="https://github.com/1umine/nonebot_plugin_charpic",
-    supported_adapters={
-        "~onebot.v11",
-        "~onebot.v12",
-        "~qq",
-        "~mirai2",
-    },
+    supported_adapters={"~onebot.v11"},
 )
 
 pic2text = on_command("字符画", priority=26, block=True)
@@ -30,16 +23,14 @@ pic2text = on_command("字符画", priority=26, block=True)
 async def _(state: T_State, args: Message = CommandArg()):
     for seg in args:
         if seg.type == "image":
-            state["image"] = [seg]
+            state["image"] = Message(seg)
 
 
 @pic2text.got("image", prompt="图呢？")
-async def generate_(bot: Bot, state: T_State):
-    msg: List[Type[MessageSegment]] = state["image"]
+async def generate_(state: T_State):
+    msg = state["image"]
     if msg[0].type == "image":
-        url: str = msg[0].data.get("url") or await pic2text.finish(
-            "无法获取图片链接"
-        )  # 获取图片链接，需要有 url 字段（别的感觉太麻烦，摆了）
+        url = msg[0].data["url"]  # 图片链接
         await pic2text.send("努力生成中...")
 
         pic = await get_img(url)  # 取图
@@ -48,10 +39,10 @@ async def generate_(bot: Bot, state: T_State):
 
         if pic.format == "GIF":
             res = await char_gif(pic)
-            await pic2text.finish(await Image(res, bot))
+            await pic2text.finish(MessageSegment.image(res))
         text = await get_pic_text(pic)
         if text:
             res = await text2img(text)
-            await pic2text.finish(await Image(res, bot))
+            await pic2text.finish(MessageSegment.image(res))
     else:
-        await pic2text.finish("没有发现图片")
+        await pic2text.finish("要的是图")
